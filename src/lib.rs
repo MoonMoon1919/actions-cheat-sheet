82ef45132b056c9dfba0943b6084ad16e47df1a1
Maxwell Moon <moon.maxwell@gmail.com>
2023-09-12 20:35:09 -0700

    add comment on PR workflow

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn hello_test() {
+        let some_string = "Hello, test!";
+
+        assert_eq!(some_string, String::from("Hello, test!"))
+    }
+}
